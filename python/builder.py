5a27444fd3a3117273fa5525a81fe443527e900d
Edoardo Bezzeccheri <e.bezzeccheri@gmail.com>
2018-04-13 14:18:44 +0200

    Even normal astra python build compiles with debug symbols, but links to release astra/cpp

--- python/builder.py
+++ python/builder.py
@@ -73,6 +73,8 @@ for source in p.glob('*.pyx'):
                                  include_dirs=[(root_p / 'include').absolute().as_posix(),
                                                (root_p / 'lib' / 'include').absolute().as_posix()],
                                  library_dirs=[(root_p / 'lib' / 'x64').absolute().as_posix()],
+                                 extra_compile_args=["-Zi", "/Od", "/MD"],
+                                 extra_link_args=["-debug"],
                                  # define_macros=[("ASTRA_CUDA", None), ("ASTRA_PYTHON", None)]
                                  ))
 for m in ext_modules:
